a27a74c97954da1b651fd809aeeb77594b63af3a
abhigya <abhigyadufare@gmail.com>
2025-02-08 03:02:27 -0500

    bug fix and create SuperCoin component

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeItemFromCart, clearCart, increaseItemQuantity, decreaseitemQuantity } from './CartSlice'; //Action creators for increasing and decreasing item quantity
+import { removeItemFromCart, clearCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice'; //Action creators for increasing and decreasing item quantity
 import './ShoppingCart.css'; 
 
 const ShoppingCart = () => {
